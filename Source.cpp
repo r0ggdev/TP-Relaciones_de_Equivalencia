fb7a4d10f5dcf7cc6dd71675948e7d1db97198fe
Dhilsen Mallqui <160343685+Dhilsen18@users.noreply.github.com>
2024-04-16 12:20:31 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,159 +1,191 @@
 #include<iostream>
 #include<conio.h>
+#include<ctime>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
-using namespace System;
 
 struct pares {
-	int x;
-	int y;
+    int x;
+    int y;
 };
 
 void setSizeA(short& size_a) {
-
-	// verifica que el valor del conjunto este dentro del rango 
-	do {
-		cout << "\n Ingrese el tama o de elementos del conjunto (entre 4 y 7): ";
-		cin >> size_a;
-	} while (size_a < 4 || size_a>7);
-
+    // verifica que el valor del conjunto esté dentro del rango
+    do {
+        cout << "\n Ingrese el tamaño de elementos del conjunto (entre 4 y 7): ";
+        cin >> size_a;
+    } while (size_a < 4 || size_a > 7);
 }
 
 void valuesOfSetA(char& verify, short& size_a, int* setA) {
+    // verifica que se seleccione una opción válida A/M
+    do {
+        cout << "\n Desea usar datos aleatorios [A] o ingresarlos manualmente [M]: ";
+        cin >> verify;
+        verify = toupper(verify);
+    } while (verify != 'A' && verify != 'M');
 
-	// verifica que se seleccione una opcion valida A/M
-	do {
-		cout << "\n Desea usar datos aleatoreos [A] o ingresarlos manulamente [M]: ";
-		cin >> verify;
-		verify = toupper(verify);
+    // realiza la opción dependiendo del valor ingresado anteriormente
+    if (verify != 'M') {
+        for (short i = 0; i < size_a; i++) {
+            // se le asigna valores del 1 al 12 al conjunto A
+            setA[i] = 1 + rand() % (12 + 1 - 1);
+            // verificación de número repetido
+            for (short j = 0; j < i; j++) {
+                if (setA[i] == setA[j]) {
+                    setA[i] = 1 + rand() % (12 + 1 - 1);
+                    j = -1;
+                }
+            }
+        }
+    }
+    else {
+        for (short i = 0; i < size_a; i++) {
+            float check_int;
+            cout << "\n Ingrese los datos del conjunto A - posición [" << i + 1 << "]: ";
+            cin >> check_int;
+            if (check_int - int(check_int) != 0) {
+                cout << "\n\n!!Número decimal, ingresa otro!!\n";
+                i--;
+            }
+            else {
+                setA[i] = check_int;
+            }
+            for (int j = 0; j < i; j++) {
+                if (setA[i] == setA[j]) {
+                    cout << "\n\n!!Número repetido, vuelve a intentar!!\n\n";
+                    cout << "Ingrese los datos del conjunto A - posición [" << i + 1 << "]: ";
+                    cin >> setA[i];
+                    j = -1;
+                }
+            }
+        }
+    }
 
-	} while (verify != 65 && verify != 77);
-
-	// realiza la opcion dependiendo del valor ingresado anteriormente 
-	if (verify != 'M') {
-
-		for (short i = 0; i < size_a; i++) {
-
-			// se le asigna valores del 1 al 12 al conjunto A
-			setA[i] = 1 + rand() % (12 + 1 - 1);
-
-			// verificacion de numero repetido
-			for (short j = 0; j < i; j++) {
-				if (setA[i] == setA[j]) {
-					setA[i] = 1 + rand() % (12 + 1 - 1);
-					j = -1;
-				}
-			}
-		}
-	}
-	else {
-
-		for (short i = 0; i < size_a; i++) {
-
-			float check_int;
-
-			Console::ForegroundColor = ConsoleColor::White;
-			cout << "\n Ingrese los datos del conjunto A - posicion [" << i + 1 << "]: ";
-			cin >> check_int;
-
-			if (check_int - int(check_int) != 0) {
-				Console::ForegroundColor = ConsoleColor::Red;
-				cout << "\n\n!!NUMERO DECIMAL, INGRESA OTRO!!\n"; i--;
-			}
-
-			else { setA[i] = check_int; }
-
-			for (int j = 0; j < i; j++) {
-
-				if (setA[i] == setA[j]) {
-					// cout << "Ya usaste este numero, gil, prueba otro: ";  cin >> setA[i];
-					Console::ForegroundColor = ConsoleColor::Red;
-					cout << "\n\n!!NUMERO REPETIDO, VUELVE A INTENTAR!!\n\n";
-
-					Console::ForegroundColor = ConsoleColor::White;
-					cout << "Ingrese los datos del conjunto A - posicion [" << i + 1 << "]: "; cin >> setA[i];
-
-					j = -1;
-				}
-			}
-
-		}
-	}
-
-	// mostrar datos
-	cout << "\n Conjunto A = { ";
-	for (short i = 0; i < size_a; i++) {
-		cout << setA[i] << " ";
-	}
-	cout << "} \n\n\n";
+    // mostrar datos
+    cout << "\n Conjunto A = { ";
+    for (short i = 0; i < size_a; i++) {
+        cout << setA[i] << " ";
+    }
+    cout << "} \n\n\n";
 }
 
 void operationSetA(short& size_a, int* setA, int** setAA, pares* setR) {
-	int valueR = 0;
-	// realiza la suma del conjunto A 
-	for (short i = 0; i < size_a; i++) {
-		for (short j = 0; j < size_a; j++) {
-			if ((setA[i] + setA[j]) % 3 == 0 || (setA[i] + setA[j]) % 4 == 0 || (setA[i] + setA[j]) % 5 == 0 || (setA[i] + setA[j]) % 6 == 0) {
-				setAA[i][j] = 1;
-				setR[valueR].x = setA[i];
-				setR[valueR].y = setA[j];
-				valueR++;
-			}
-			else setAA[i][j] = 0;
-		}
-	}
+    int valueR = 0;
+    // realiza la suma del conjunto A
+    for (short i = 0; i < size_a; i++) {
+        for (short j = 0; j < size_a; j++) {
+            if ((setA[i] + setA[j]) % 3 == 0 || (setA[i] + setA[j]) % 4 == 0 || (setA[i] + setA[j]) % 5 == 0 || (setA[i] + setA[j]) % 6 == 0) {
+                setAA[i][j] = 1;
+                setR[valueR].x = setA[i];
+                setR[valueR].y = setA[j];
+                valueR++;
+            }
+            else setAA[i][j] = 0;
+        }
+    }
 
-	// mostrar datos 
-	for (short i = 0; i < size_a; i++) {
-		for (short j = 0; j < size_a; j++) {
-			cout << "(A" << i + 1 << ",A" << j + 1 << "): " << setA[i]+ setA[j];
-			if (j < size_a - 1)cout << "  ";
-		}
-		cout << endl;
-	}
+    // mostrar datos
+    cout << "\n Mostrar matriz:\n\n";
+    for (short i = 0; i < size_a; i++) {
+        for (short j = 0; j < size_a; j++) {
+            cout << "[" << setAA[i][j] << "]";
+        }
+        cout << endl;
+    }
 
-	cout << "\n Mostrar matriz:\n\n";
-	for (short i = 0; i < size_a; i++) {
-		for (short j = 0; j < size_a; j++) {
-			cout << "[" << setAA[i][j] << "]";
-		}
-		cout << endl;
-	}
+    cout << "\n\n Conjunto R1: { ";
+    for (short i = 0; i < valueR; i++) {
+        cout << "(" << setR[i].x << "," << setR[i].y << ")";
+        if (i < valueR - 1)cout << ",";
+    }
+    cout << "}";
 
-	cout << "\n\n Conjunto R1: { ";
-	for (short i = 0; i < valueR; i++) {
-		cout << "(" << setR[i].x << "," << setR[i].y << ")";
-		if (i < valueR - 1)cout << ",";
-	}
-	cout << "}";
+    // Verificar si la relación es reflexiva, simétrica, transitiva y de equivalencia
+    bool reflexiva = true, simetrica = true, transitiva = true;
+    for (short i = 0; i < size_a; i++) {
+        if (setAA[i][i] != 1) reflexiva = false;
+        for (short j = 0; j < size_a; j++) {
+            if (setAA[i][j] != setAA[j][i]) simetrica = false;
+            for (short k = 0; k < size_a; k++) {
+                if (setAA[i][j] && setAA[j][k] && !setAA[i][k]) transitiva = false;
+            }
+        }
+    }
+
+    if (reflexiva) cout << "\n\n La relacion es reflexiva.";
+    else cout << "\n\n La relacion no es reflexiva.";
+
+    if (simetrica) cout << "\n La relacion es simetrica.";
+    else cout << "\n La relacion no es simetrica.";
+
+    if (transitiva) cout << "\n La relacion es transitiva.";
+    else cout << "\n La relacion no es transitiva.";
+
+    if (reflexiva && simetrica && transitiva) {
+        cout << "\n\n Es una relacion de equivalencia.\n";
+        // Presentar la clase de equivalencia de cada elemento
+        cout << "\n Clases de equivalencia:\n";
+        vector<vector<int>> clases_equivalencia;
+        bool* visitado = new bool[size_a]();
+        for (short i = 0; i < size_a; i++) {
+            if (!visitado[i]) {
+                vector<int> clase;
+                clase.push_back(setA[i]);
+                visitado[i] = true;
+                for (short j = i + 1; j < size_a; j++) {
+                    if (setAA[i][j]) {
+                        clase.push_back(setA[j]);
+                        visitado[j] = true;
+                    }
+                }
+                sort(clase.begin(), clase.end());
+                clases_equivalencia.push_back(clase);
+            }
+        }
+
+        // Mostrar las clases de equivalencia
+        for (int i = 0; i < clases_equivalencia.size(); i++) {
+            cout << " Clase " << i + 1 << ": { ";
+            for (int j = 0; j < clases_equivalencia[i].size(); j++) {
+                cout << clases_equivalencia[i][j] << " ";
+            }
+            cout << "}\n";
+        }
+
+        delete[] visitado;
+    }
+    else {
+        cout << "\n\n No es una relacion de equivalencia.\n";
+    }
 }
 
 int main() {
-		srand(time(NULL)); // semilla de tiempo
+    srand(time(NULL)); // semilla de tiempo
 
-		// declara las variables
-		short size_a = 0; // tamaño del conjunto A
-		char verify = ' '; // almacena A / M  
+    // declara las variables
+    short size_a = 0; // tamaño del conjunto A
+    char verify = ' '; // almacena A / M  
 
-		// se obtiene el tamanio del conjunto A
-		setSizeA(size_a);
-		int* setA = new int[size_a]; // se crea el conjunto A
-		pares* setR = new pares[size_a * size_a]; // se crea el conjunto R
+    // se obtiene el tamaño del conjunto A
+    setSizeA(size_a);
+    int* setA = new int[size_a]; // se crea el conjunto A
 
-		int** setAA = new int* [size_a]; // se crea la matriz dinamica 
-		for (short i = 0; i < size_a; i++) { setAA[i] = new int[size_a]; } // se declara las columnas de la matriz
+    int** setAA = new int* [size_a]; // se crea la matriz dinámica
+    for (short i = 0; i < size_a; i++) { setAA[i] = new int[size_a]; } // se declaran las columnas de la matriz
 
-		// declara operaciones
-		valuesOfSetA(verify, size_a, setA);
-		operationSetA(size_a, setA, setAA, setR);
+    pares* setR = new pares[size_a * size_a]; // se crea el conjunto R
 
+    // declara operaciones
+    valuesOfSetA(verify, size_a, setA);
+    operationSetA(size_a, setA, setAA, setR);
 
-		// se libera la memoria
+    // libera la memoria
+    for (int i = 0; i < size_a; i++) { delete[] setAA[i]; }
+    delete[] setAA, setA, setR;
 
-		for (int i = 0; i < size_a; i++) { delete[] setAA[i]; }
-		delete[] setAA, setA, setR;
-
-
-		_getch();
-	return 0;
+    _getch();
+    return 0;
 }
