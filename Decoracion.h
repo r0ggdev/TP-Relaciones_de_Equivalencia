b4adc3fc64666786ca97787bd50d1b7297b11178
Claus <165341343+Cl4us-tb@users.noreply.github.com>
2024-05-11 14:42:04 -0500

    Update Decoracion.h

--- Decoracion.h
+++ Decoracion.h
@@ -86,11 +86,14 @@ public:
     }
 
     void numeros(int x, int y) {
+        Console::CursorVisible = false;
         Random r;
         for (int i = 0; i < 6; i++) {
-            Console::SetCursorPosition(x+22+(i*3), y + 8 + i);
-            cout << r.Next(0, 2);
-            _sleep(400);
+            for (int j = 0; j < 6; j++) {
+                Console::SetCursorPosition(x + 22 + (j * 3), y + 8 + i);
+                cout << r.Next(0, 2);
+                _sleep(90);
+            }
         }
 
         Console::SetCursorPosition(x + 21, y + 15);
@@ -100,6 +103,70 @@ public:
             Console::SetCursorPosition(x + 30 + (i * 1), y +15);
             cout <<".";
         }
+    }
+
+    void marco(int width, int height) {
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (i == 0 && j == 0)cout <<"  "<< char(218);
+                else if (i == height - 1 && j == 0)cout << "  "<<char(192);
+                else if (i == 0 && j == width - 1)cout << char(191);
+                else if (i == height - 1 && j == width - 1)cout << char(217);
+                else if (i == 0 || i == height - 1)cout <<char(196);
+                else if (j == 0 || j == width - 1) { 
+                    if (j == 0)cout << "  ";
+                    cout << char(179); }
+                else cout << " ";
+            }
+            if(i< height-1)cout << endl;
+        }
+
+    }
+
+    void matriz(int width, int x, int y, int** setAA, int* setA){
+        Console::SetCursorPosition(x, y);
+        cout << "Mostrar matriz:";
+        short size = width;
+        short height = width + 2;
+        width = width + (width - 1) + 4;
+
+        Console::SetCursorPosition(x+3, y + 2);
+        for (short i = 1; i < size+1; i++) {
+            cout << setA[i-1]<<" ";
+        }
+
+        for (short i = 1; i < size+1; i++) {
+            Console::SetCursorPosition(x-1, y + 3+i);
+            cout << setA[i-1];
+        }
+
+        for (int i = 0; i < height; i++) {
+
+            for (int j = 0; j < width; j++) {
+                Console::SetCursorPosition(x + j+1, y+3 + i);
+                if (i == 0 && j == 0) { cout << char(218); }
+
+                else if (i == height - 1 && j == 0) { cout << char(192); }
+
+                else if (i == 0 && j == width - 1) { cout << char(191); }
+
+                else if (i == height - 1 && j == width - 1) { cout << char(217); }
+
+                else if (j == 0 || j == width - 1) {
+                    cout << char(179);
+                }
+                else { cout << " "; }
+            }
+        }
+
+        for (short i = 0; i < size; i++) {
+            for (short j = 0; j < size; j++) {
+                Console::SetCursorPosition(x+3 + (2*j), y+4 + i);
+                cout << setAA[i][j];
+            }
+        }
+
     }
 	~Deco();
 
